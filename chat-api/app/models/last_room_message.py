42e6257493471a800bef76c7a96a4053b843f1cd
m4reQ <m.mizerski2004@gmail.com>
2025-12-29 22:58:24 +0100

    Fix last chat room message subquery

--- chat-api/app/models/last_room_message.py
+++ chat-api/app/models/last_room_message.py
@@ -4,7 +4,7 @@ from app.models.message import SQLMessage
 from app.models.user import SQLUser
 from app.models.sql import Base
 
-_last_message_subquery = (
+_last_message_subq = (
     sqlalchemy.select(
         SQLMessage.id,
         SQLMessage.room_id,
@@ -13,19 +13,23 @@ _last_message_subquery = (
         SQLMessage.sent_at,
         SQLMessage.sender_id,
         SQLUser.username.label('sender_username'),
+        sqlalchemy.func.row_number()
+        .over(
+            partition_by=SQLMessage.room_id,
+            order_by=SQLMessage.sent_at.desc()
+        )
+        .label("rn")
     )
     .join(SQLUser, SQLUser.id == SQLMessage.sender_id)
-    .where(
-        SQLMessage.sent_at == sqlalchemy.select(
-            sqlalchemy.func.max(SQLMessage.sent_at)
-        )
-        .where(SQLMessage.room_id == SQLMessage.room_id)
-        .correlate(SQLMessage)
-        .scalar_subquery()
-    )
+    .subquery()
+)
+
+_last_message_per_room = (
+    sqlalchemy.select(_last_message_subq)
+    .where(_last_message_subq.c.rn == 1)
     .subquery()
 )
 
 class SQLLastRoomMessage(Base):
-    __table__ = _last_message_subquery
-    __mapper_args__ = {'primary_key': [_last_message_subquery.c.id]}
+    __table__ = _last_message_per_room
+    __mapper_args__ = {'primary_key': [_last_message_per_room.c.id]}
